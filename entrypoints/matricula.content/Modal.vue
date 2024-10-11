61a2503e4d4025c3026bb6f206d778217d7b4b84
Joabesv <joabevarjao123@gmail.com>
2024-10-11 16:53:17 -0300

    wip: tryna communicate

---
+++ entrypoints/matricula.content/Modal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+type ModalState = {
+  isOpen: boolean;
+  corteId: string | null;
+};
+
+type ModalProps = {
+  modalState: ModalState;
+  onClose: () => void;
+};
+
+const props = defineProps<ModalProps>();
+console.log('porra', props)
+const show = computed(() => props.modalState.isOpen);
+
+// const closeModal = () => {
+//   props.onClose();
+// };
+</script>
+
+
+<template>
+  <div v-if="show">
+    <el-dialog title="Corte Details" :visible="show" light class="w-[712px]">
+      Corte ID: {{ modalState.corteId }}
+      <!-- Add more content here based on the corteId -->
+    </el-dialog>
+  </div>
+</template>
--- entrypoints/matricula.content/index.ts
+++ entrypoints/matricula.content/index.ts
@@ -1,4 +1,7 @@
 import Matricula from "./Matricula.vue";
+import Modal from "./Modal.vue";
+import TeacherReview from "./TeacherReview.vue";
+import SubjectReview from "./SubjectReview.vue";
 import type { ContentScriptContext } from "wxt/client";
 import "./style.css";
 import { getUFEnrolled } from "@/services/UFParser";
@@ -17,6 +20,12 @@ export default defineContentScript({
 		mountedUi.style.position = "sticky";
 		mountedUi.style.top = "0px";
 		mountedUi.style.zIndex = "9";
+
+		const modal = await mountModal(ctx);
+		modal.mount();
+		await mountTeacherReview(ctx);
+		await mountSubjectReview(ctx);
+		addEventListeners();
 	},
 	runAt: "document_end",
 	cssInjectionMode: "ui",
@@ -49,5 +58,111 @@ async function mountMatriculaFilters(ctx: ContentScriptContext) {
 			mounted?.app.unmount();
 			mounted?.wrapper.remove();
 		},
+	});
+}
+
+async function mountModal(ctx: ContentScriptContext) {
+	return createShadowRootUi(ctx, {
+		name: "kicked-modal",
+		position: "modal",
+		anchor: "#menu",
+		append: "first",
+		onMount(container) {
+			const wrapper = document.createElement("div");
+			container.append(wrapper);
+			const app = createApp(Modal, {
+				modalState: modalState.value,
+				onClose: () => {
+					modalState.value.isOpen = false;
+					modalState.value.corteId = null;
+				},
+			});
+
+			app.mount(wrapper);
+			return { app, wrapper };
+		},
+		onRemove(mounted) {
+			mounted?.app.unmount();
+			mounted?.wrapper.remove();
+		},
+	});
+}
+
+async function mountTeacherReview(ctx: ContentScriptContext) {
+	return createShadowRootUi(ctx, {
+		name: "teacher-review",
+		position: "inline",
+		anchor: "body",
+		onMount(container) {
+			const wrapper = document.createElement("div");
+			container.append(wrapper);
+			const app = createApp(TeacherReview);
+			app.mount(wrapper);
+			return { app, wrapper };
+		},
+		onRemove(mounted) {
+			mounted?.app.unmount();
+			mounted?.wrapper.remove();
+		},
+	});
+}
+
+async function mountSubjectReview(ctx: ContentScriptContext) {
+	return createShadowRootUi(ctx, {
+		name: "subject-review",
+		position: "inline",
+		anchor: "body",
+		onMount(container) {
+			const wrapper = document.createElement("div");
+			container.append(wrapper);
+			const app = createApp(SubjectReview);
+			app.mount(wrapper);
+			return { app, wrapper };
+		},
+		onRemove(mounted) {
+			mounted?.app.unmount();
+			mounted?.wrapper.remove();
+		},
+	});
+}
+
+const modalState = ref<{ isOpen: boolean; corteId: null | string }>({
+	isOpen: false,
+	corteId: null,
+});
+const teacherReviewState = ref({
+	isOpen: false,
+	teacherId: null,
+	teacherName: null,
+});
+const subjectReviewState = ref({ isOpen: false, subjectId: null });
+
+function addEventListeners() {
+	document.body.addEventListener("click", (e) => {
+		const target = e.target as HTMLSpanElement;
+		if (target.closest("#cortes")) {
+			const corteElement = target.closest("#cortes");
+			if (!corteElement) {
+				return;
+			}
+			const corteId =
+				corteElement.parentElement?.parentElement?.getAttribute("value");
+			if (corteId) {
+				console.log("here fuck", modalState.value);
+				modalState.value.isOpen = true;
+				modalState.value.corteId = corteId;
+			}
+		}
+
+		if (target.classList.contains("ReviewTeacher")) {
+			const teacherId = target.getAttribute("data");
+			const teacherName = target.getAttribute("teacherName");
+			// Trigger teacher review open with teacherId and teacherName
+		}
+
+		if (target.matches("span.sa, span.sbc")) {
+			const subjectId = target.getAttribute("subjectId");
+			// Trigger subject review open with subjectId
+		}
 	});
 }
